ca10d87ab9ee3cfcaa7ca9aa2cc15d7feea1c857
Salomander <s.lacunes@gmail.com>
2021-11-19 20:33:40 +0100

    Update MCP.py

--- MCP.py
+++ MCP.py
@@ -1,6 +1,12 @@
 import numpy as np
-
+"""
+kript struct:
+Adj_M = {program key: adjacency matrix}
+State_V = {proposition key: interpretation vec}
+"""
 class Kripke:
     def __init__(self, Adj_M, State_V):
         self.Adj_M = Adj_M
         self.State_V = State_V
+        self.atoms = State_V.keys()
+        self.programs = Adj_M.keys()
