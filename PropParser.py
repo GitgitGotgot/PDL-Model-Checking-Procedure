db82ea37de545813192fa96ff59cfad23d3369ca
Salomander <s.lacunes@gmail.com>
2021-12-13 23:03:39 +0100

    Update PropParser.py

--- PropParser.py
+++ PropParser.py
@@ -12,14 +12,14 @@ class PDLparser():
         #self.unary_operators = ['!', '?']
         #self.unary_operators = {'!':False, '?':True}
         self.negation = False
-        self.valid_prop_op = ['&']
+        self.valid_prop_op = ['&', '/']
         self.valid_prog_op = [';','U', 'X']
         self.active_program = False
         self.unary_brac = False
 
     # for future -> operator
     # maybe first strip string into literals [(,q,&,b,),->,c] then iterate
-    def parse2(self, i_s):
+    def parse(self, i_s):
         stack = [[]]
         for x in i_s:
             if x == '!':
@@ -39,7 +39,7 @@ class PDLparser():
                     return 'error: misaligned program brackets'
                 self.open_brac[self.close_brac[x]] = False
                 self.active_program = False
-            elif x == '*' or x == '?' or x == 'c':
+            elif x == '+' or x == '*' or x == '?' or x == "'":
                 if not self.active_program:
                     return 'error: program operator used outside program'
                 else:
@@ -83,53 +83,22 @@ class PDLparser():
             return 'error: invalid formula'
         return stack.pop()
 
-    def parse_rec(self, i_s):
-        def help_func(level=0):
-            try:
-                token = next(tokens)
-                #print(token)
-            except StopIteration:
-                    return []
-            if token == ')' or token == '>' or token == ']':
-                if level == 0:
-                    raise Exception('missing opening paren')
-                else:
-                    #if unary_brac:
-                        #unary_brac = False
-                    return []
-            elif token == '(':
-                #if unary:
-                    #unary = False
-                    #return [help_func(level+1, unary_brac=True)] + help_func(level)
-                return [help_func(level+1)] + help_func(level)
-            elif token == '<' or token == '[':
-                return [token, help_func(level+1)] + help_func(level)
-            elif token == '!':
-                #return [token, help_func(level+1, unary=True)] + help_func(level)
-                return [token, help_func(level+1)] + help_func(level)
-            else:
-                #if unary:
-                    #unary = False
-                    #return [['!', token]] + help_func(level)
-                return [token] + help_func(level)
-        tokens = iter(i_s)
-        return help_func()
-
+"""
 while True:
     c = PDLparser(['p','q','r','s'],['a','b','c','d'])
     # !(p&q) Or (!p & !q)
     s = input("Enter a logical formula: ")
     #ret = c.parse_rec(s)
-    ret = c.parse2(s)
+    ret = c.parse(s)
     print(ret)
-
+"""
     #print(ret[0])
     #print(ret[0][1])
     #print(ret[0][1][2])
     #print(ret[0][1][2][0])
     #print(ret2[1])
     #print(ret2[1][2])
-    #print(c.parse2(s))
+    #print(c.parse(s))
     #print(c.foo(s))
 
 #print(foo(s))
