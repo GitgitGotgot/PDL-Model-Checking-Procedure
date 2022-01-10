38f417d9425b90e97dbcc6021cb46a2e75ddbee8
Salomander <s.lacunes@gmail.com>
2022-01-10 21:08:53 +0100

    Update MCP.py

--- MCP.py
+++ MCP.py
@@ -1,6 +1,8 @@
 import numpy as np
+from scipy import sparse
 from PropParser import PDLparser
 from ModelGen import ModelGen
+
 """
 kript struct:
 Adj_M = {program key: adjacency matrix}
@@ -13,136 +15,151 @@ function that checks if formula is valid in current adjacency matrix
 [a;b*](q&!p)
 'NoneType'
 <!a> wordt [<,[]]
+
+a.multiply(b) #AND
+a+b           #OR
+(a>b)+(a<b)   #XOR
+a>b           #NOT
 """
-# Adj_M = {}
-# Adj_M['a'] = np.array([[0,1,0,0,0],
-#               [0,1,0,0,0],
-#               [0,0,0,0,0],
-#               [0,0,0,0,0],
-#               [0,0,0,0,0]], dtype=bool)
-# Adj_M['b'] = np.array([[0,0,0,0,0],
-#               [0,0,1,1,0],
-#               [0,0,0,0,0],
-#               [0,0,0,0,0],
-#               [0,0,0,0,0]], dtype=bool)
-# Adj_M['c'] = np.array([[0,0,0,0,1],
-#               [0,0,0,0,1],
-#               [0,0,0,0,0],
-#               [0,0,0,0,0],
-#               [0,0,0,0,0]], dtype=bool)
-# State_V = {}
-# State_V['p'] = np.array([0,1,0,1,0], dtype=bool)
-# State_V['q'] = np.array([0,1,1,0,1], dtype=bool)
-# State_V['q'] = np.array([0,1,0], dtype=bool)
+
 class Kripke:
-    def __init__(self, Adj_M, State_V):
+    def __init__(self, Adj_M, State_V, N):
         self.Adj_M = Adj_M
         self.State_V = State_V
         self.atoms = State_V.keys()
         self.programs = Adj_M.keys()
+        self.states = N
     def Input_Function(self, input_formula=False, tests=False):
         formula = PDLparser(self.atoms, self.programs).parse(input_formula)
-        #print(self.MCP(formula))
         print('test: ', formula)
         self.MCP(formula)
-        # return any true from MCP
 
     def MCP(self, formula):
         if isinstance(formula, list):
             if len(formula) == 1:
                 return self.MCP(formula[0])
             if formula[0] == '<':
-                # prog x formula diamond version and box version
                 print('formula')
                 print(self.MCP(formula[2]).astype(int))
                 print('prog')
                 print(self.Prog(formula[1]).astype(int))
                 print('ret')
-                print(self.diamond_op(self.Prog(formula[1]), self.MCP(formula[2])).astype(int))
+                # print((self.Prog(formula[1]) @ self.MCP(formula[2])).astype(bool))
+                # print(self.Prog(formula[1]).shape + self.MCP(formula[2]).shape)
                 print('')
-                return self.diamond_op(self.Prog(formula[1]), self.MCP(formula[2]))
+                #return self.diamond_op(self.Prog(formula[1]), self.MCP(formula[2]))
+                return (self.Prog(formula[1]) @ self.MCP(formula[2])).astype(bool)
             if formula[0] == '[':
                 # print('formula')
-                print('formula')
-                print((self.MCP(formula[2])^1).astype(int))
-                print('prog')
-                print((self.Prog(formula[1])).astype(int))
-                print('ret')
-                print(self.diamond_op(self.Prog(formula[1]), self.MCP(formula[2])^1)^1)
+                # print((self.MCP(formula[2])^1).astype(int))
+                # print('prog')
+                # print((self.Prog(formula[1])).astype(int))
+                # print('ret')
+                print(((self.Prog(formula[1]) @ self.MCP(formula[2])^1)^1).astype(bool))
                 print('')
-                return (self.diamond_op(self.Prog(formula[1]), self.MCP(formula[2])^1)^1).astype(bool)
+                #return (self.diamond_op(self.Prog(formula[1]), self.MCP(formula[2])^1)^1).astype(bool)
+                return ((self.Prog(formula[1]) @ self.MCP(formula[2])^1).astype(bool)^1).astype(bool)
+            if formula[0] == 'R':
+                prog = self.Prog(formula[1])
+                if sparse.issparse(prog):
+                    return prog.diagonal()
+                return np.diag(prog)
+            if formula[0] == 'L':
+                prog = self.kleene_plus(self.Prog(formula[1])).astype(bool)
+                if sparse.issparse(prog):
+                    # print('s[arse]')
+                    # print(np.diag(prog).shape)
+                    # print((self.Adj_M['IDENTITY'] + prog).shape)
+                    # bla = (self.Adj_M['IDENTITY'] + prog) @ np.diag(prog)
+                    # print(bla.shape)
+                    return ((self.Adj_M['IDENTITY'] + prog).astype(bool) @ prog.diagonal()).astype(bool)
+                #print(((self.Adj_M['IDENTITY'] + prog).astype(bool) @ np.diag(prog)).astype(bool))
+                # print('donse')
+                # print(prog.diagonal().shape)
+                # print((self.Adj_M['IDENTITY'] + prog).shape)
+                # bla = (self.Adj_M['IDENTITY'] + prog) @ prog.diagonal()
+                # bla = np.squeeze(bla)
+                # print(bla.shape)
+                return ((self.Adj_M['IDENTITY'] + prog).astype(bool) @ np.diag(prog)).astype(bool)
             if formula[0] == '!':
-                return self.MCP(formula[1])^1
+                return (self.MCP(formula[1])^1).astype(bool)
             if formula[1] == '&':
-                return self.MCP(formula[0]) & self.MCP(formula[2])
+                return (self.MCP(formula[0]) * self.MCP(formula[2])).astype(bool)
             if formula[1] == '/':
-                return self.MCP(formula[0]) | self.MCP(formula[2])
+                return (self.MCP(formula[0]) + self.MCP(formula[2])).astype(bool)
+            if formula[1] == '->':
+                return ((self.MCP(formula[0])^1).astype(bool) + self.MCP(formula[2])).astype(bool)
         else:
+            print(State_V[formula])
             return State_V[formula]
 
     def Prog(self, program):
-        #print('program:', program)
         if isinstance(program, list):
             if len(program) == 1:
                 return self.Prog(program[0])
             if program[0] == '!':
-                return self.Prog(program[1])^1
+                prog = self.Prog(program[1])
+                if sparse.issparse(prog):
+                    return (prog.A^1).astype(bool)
+                return (prog^1).astype(bool)
             if program[0] == "'":
-                return np.transpose(self.Prog(program[1]))
+                return self.Prog(program[1]).transpose()
             if program[0] == '+':
                 prog = self.Prog(program[1])
                 return self.kleene_plus(prog)
             if program[0] == '*':
-                prog = self.Prog(program[1])
-                return np.identity(len(prog), dtype=bool) | self.kleene_plus(prog)
+                prog = self.Prog(program[1]).astype(bool)
+                return (self.Adj_M['IDENTITY'] + self.kleene_plus(prog)).astype(bool)
             if program[0] == '?':
                 return np.diag(self.MCP(program[1]))
-                #return np.identity(len(prog), dtype=bool) | self.kleene_plus(prog)
             if program[1] == ';':
-                return self.m_composition(self.Prog(program[0]), self.Prog(program[2]))
+                return (self.Prog(program[0]) @ self.Prog(program[2])).astype(bool)
             if program[1] == 'U':
-                return self.Prog(program[0]) | self.Prog(program[2])
+                return (self.Prog(program[0]) + self.Prog(program[2])).astype(bool)
             if program[1] == 'X':
-                return self.Prog(program[0]) & self.Prog(program[2])
+                return (self.Prog(program[0]) * self.Prog(program[2])).astype(bool)
         else:
             return self.Adj_M[program]
 
-    def m_composition(self, m1, m2):
-        ret = np.zeros((len(m1), len(m1)), dtype=bool)
-        for i in range(len(m1)):
-            for j in range(len(m1[i])):
-                # print(np.any(np.transpose(m2)[j]))
-                # print( np.any(m1[i]))
-                # print(ret[i][j])
-                #if np.any(m1[i])
-                ret[i][j] = ret[i][j] | (np.any(m1[i]) & np.any(np.transpose(m2)[j]))
-        # for i in range(len(m1)):
-        #     if not 1 in m1[i]:
-        #         m2[i].fill(0)
-        # return m2
-        return ret
-
-    def diamond_op(self, m1, v1):
-        ret = np.zeros((len(v1)), dtype=bool)
-        for i in range(len(m1)):
-            for j in range(len(m1[i])):
-                ret[i] = ret[i] | (m1[i][j] & v1[j])
-        return ret
     def kleene_plus(self, program):
         prog = program
         ret = program
         # for i in range(random.randint(1, 20)):
-        for i in range(len(program)^2):
-            prog = self.m_composition(prog, program)
-            ret = ret | prog
-        return ret
+        for i in range(self.states^2):
+            prog = (prog @ program).astype(bool)
+            ret = (ret + prog).astype(bool)
+        return ret.astype(bool)
 
-State_V, Adj_M, Tests = ModelGen('Test.txt')
-k = Kripke(Adj_M, State_V)
+#N, State_V, Adj_M, Tests = ModelGen(file='Test.txt', sparse_matrix=False)
+#N, State_V, Adj_M, Tests = ModelGen(gen=True, sparse_matrix=True)
+N, State_V, Adj_M, Tests = ModelGen(gen=True, states=100, progs = 3, props = 3, sparse_matrix=True)
+# N, State_V, Adj_M, Tests = ModelGen(gen=True, sparse_matrix=True)
+Adj_M2 = {}
+for key in Adj_M.keys():
+    Adj_M2[key] = Adj_M[key].A
+    # print(Adj_M2[key].shape)
+k2 = Kripke(Adj_M2, State_V, N)
+k = Kripke(Adj_M, State_V, N)
 while True:
     s = input("Enter a logical formula: ")
     if s == 'T':
+        if len(Tests) == 0:
+            print('Model has no available tests')
         for t in Tests:
             k.Input_Function(t)
+    elif s == 'H':
+        print("(Nested) formulas must always be between parentheses\n"
+              "Program operators between program brackets (<...> or [...])\n"
+              "EXAMPLE: <a;(bUc)>(p->q)\n\n"
+              "Formula Operators:\n"
+              "Loop = L(insert_program)\n"
+              "Repeat = R(insert_program)\n"
+              "Logical OR = /\n"
+              "Program Operators:\n"
+              "Test = (insert_formula)?\n"
+              "Disjuntion = X\n\n"
+              "To run all tests in the Kripke model file, insert 'T' "
+             )
     else:
-        k.Input_Function(s)
+        #k.Input_Function(s)
+        print(k.Input_Function(s) == k2.Input_Function(s))
