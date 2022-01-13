9b0cc1117630d4a2c5d1fa5eb380d7fb7dbfb5bc
Salomander <s.lacunes@gmail.com>
2022-01-13 22:20:20 +0100

    Create main.py
    Made main file (user input)

---
+++ main.py
@@ -0,0 +1,108 @@
+import timeit
+import sys
+import PropParser as parse
+import ModelGen as gen
+from MCP import Kripke
+
+def main(argv):
+    RunTests = False
+    sparse=False
+    if len(argv) < 3:
+        raise Exception("Please enter the model you want to check:\n"
+                        "To generate a model via text file enter: python main.py --file \"file_name.txt\"\n"
+                        "To run the given tests in a text file enter: python main.py --file \"file_name.txt\" --T\n"
+                        "To generate a random model enter: python main.py --random \"model_size\" \"props\"(optional) \"progs\"(optional)\n"
+                        "To generate a model that forms a straight line enter: python main.py --line \"model_size\" \"p_loc\"\n"
+                        "To generate a model that forms a circle enter: python main.py --circle \"model_size\" \"p_loc\"\n\n"
+                        "For sparse matrix structures add --sparse at the end of the command line\n\n"
+                        "Input for model size or p_loc will set to default (50 and 0 respectively) if they're not a positive integer\n"
+                        )
+        exit(-1)
+    if argv[-1] == '--sparse':
+        sparse=True
+
+    if argv[1] == "--file":
+        try:
+            f = open(argv[2])
+            f.close()
+        except FileNotFoundError:
+            print('File does not exist or wrong input')
+        N, State_V, Adj_M, Tests = gen.ModelFromFile(argv[2], sparse_matrix=sparse)
+        if len(argv) > 3:
+            if argv[3] == "--T":
+                RunTests = True
+
+    elif argv[1] == "--random":
+        N = int(argv[2]) if (int(argv[2]) > 0) else 50
+        State_V, Adj_M = gen.RandomModel(N, sparse_matrix=sparse)
+
+    elif argv[1] == "--line":
+        N = int(argv[2]) if (int(argv[2]) > 0) else 50
+        if len(argv) > 3:
+            p_loc = argv[3] if (int(argv[3]) % 1 == 0 and int(argv[3]) < N and int(argv[3]) > 0) else False
+        State_V, Adj_M = gen.LineModel(N, p_loc, sparse_matrix=sparse)
+
+    elif argv[1] == "--circle":
+        N = int(argv[2]) if (int(argv[2]) > 0) else 50
+        if len(argv) > 3:
+            p_loc = argv[3] if (int(argv[3]) % 1 == 0 and int(argv[3]) < N and int(argv[3]) > 0) else False
+        State_V, Adj_M = gen.CircleModel(N, p_loc, sparse_matrix=sparse)
+
+    elif argv[1] =="--h":
+        print("Please enter the model you want to check:\n"
+              "To generate a model via text file enter: python main.py --file \"file_name.txt\"\n"
+              "To run the given tests in a text file enter: python main.py --file \"file_name.txt\" --T\n"
+              "To generate a random model enter: python main.py --random \"model_size\" \"props\"(optional) \"progs\"(optional)\n"
+              "To generate a model that forms a straight line enter: python main.py --line \"model_size\" \"p_loc\"\n"
+              "To generate a model that forms a circle enter: python main.py --circle \"model_size\" \"p_loc\"\n\n"
+              "For sparse matrix structures add --sparse at the end of the command line\n\n"
+              )
+    # Generate the model
+    k = Kripke(Adj_M, State_V, N)
+
+    # Run tests from file
+    if RunTests:
+        if len(Tests) == 0:
+            print('Model has no available tests')
+        else:
+            for t in Tests:
+                formula = parse.FormulaParser(t, State_V.keys(), Adj_M.keys())
+                if not formula:
+                    print("Invalid formula in Tests.")
+                else:
+                    print("Test:" + str(t))
+                    print(k.MCP(formula))
+    else:
+        while True:
+            s = input("Enter a PDL formula: ")
+            if s == 'h':
+                print("Compound formulas and programs must always be between parentheses\n"
+                      "EXAMPLE: <a;(bUc)>(p->q)\n\n"
+                      "Formula Operators:\n"
+                      "<a>p = <a>p\n"
+                      "[a]p = [a]p\n"
+                      "Loop(a) = L(a)\n"
+                      "Repeat(a) = R(a)\n"
+                      "Negation(p) = !p\n"
+                      "Logical AND = &\n"
+                      "Logical OR = /\n"
+                      "Implication = ->\n\n"
+                      "Program Operators:\n"
+                      "Complement(a) = !a\n"
+                      "Test(p) = (p)?\n"
+                      "Converse(p) = (p)'\n"
+                      "Kleene_Plus(a) = a+\n"
+                      "Kleene_Star(a) = a*\n"
+                      "Composition = ;\n"
+                      "Union = U\n"
+                      "Intersection = X\n\n"
+                      "To run all tests in the Kripke model file, insert 'T' "
+                      )
+            formula = parse.FormulaParser(s, State_V.keys(), Adj_M.keys())
+            if not formula:
+                print("Press h for help.")
+            else:
+                print(k.MCP(formula))
+        #              )
+if __name__ == '__main__':
+    main(sys.argv)
