7a0c8ba7e818b6b59d517192474c77842b706741
Salomander <s.lacunes@gmail.com>
2022-01-17 20:58:07 +0100

    Update PropParser.py
    <-> added

--- PropParser.py
+++ PropParser.py
@@ -14,9 +14,10 @@ def FormulaParser(i_s, atoms, programs):
     close_brac = {'>':'<', ']':'[', ')':'('}
     unary_op = {'!':False, '?':False}
     imp_arrow = False
+    bimp_arrow = False
     negation = False
-    valid_prop_op = ['&', '/']
-    valid_prog_op = [';','U', 'X', 'R', 'L']
+    valid_prop_op = ['&', '/', 'R', 'L', 'T', 'F']
+    valid_prog_op = [';','U', 'X']
     active_program = False
     stack = [[]]
     i_s = i_s.replace(" ", "")
@@ -28,7 +29,15 @@ def FormulaParser(i_s, atoms, programs):
             if imp_arrow:
                 print('error: invalid formula, expected implication arrow due to -')
                 return False
-            imp_arrow = True
+            if open_brac['<']:
+                active_program = False
+                bimp_arrow = True
+                open_brac[x] = False
+                # undo the extend and append activated by '<'
+                stack.pop()
+                stack = [[stack[0][0]]]
+            else:
+                imp_arrow = True
         elif x == '<' or x == '[':
             open_brac[x] = True
             active_program = True
@@ -38,6 +47,9 @@ def FormulaParser(i_s, atoms, programs):
             if imp_arrow and x == '>':
                 stack[-1].append('->')
                 imp_arrow = False
+            elif bimp_arrow and x == '>':
+                stack[-1].append('<->')
+                bimp_arrow = False
             else:
                 stack.pop()
                 if len(stack[-1][-1]) > 3:
@@ -91,84 +103,7 @@ def FormulaParser(i_s, atoms, programs):
         return False
     return stack.pop()
 
-class PDLparser():
-    def __init__(self, atoms, programs):
-        self.atoms = atoms
-        self.programs = programs
-        self.open_brac = {'<':False, '[':False, '(':False}
-        self.close_brac = {'>':'<', ']':'[', ')':'('}
-        self.unary_op = {'!':False, '?':False}
-        self.imp_arrow = False
-        self.negation = False
-        self.valid_prop_op = ['&', '/']
-        self.valid_prog_op = [';','U', 'X', 'R', 'L']
-        self.active_program = False
 
-    def parse(self, i_s):
-        stack = [[]]
-        i_s = i_s.replace(" ", "")
-        print(i_s)
-        for x in i_s:
-            if x == '!':
-                self.negation = True
-            elif x == '-':
-                if self.imp_arrow:
-                    return 'error: invalid formula, expected implication arrow due to -'
-                self.imp_arrow = True
-            elif x == '<' or x == '[':
-                self.open_brac[x] = True
-                self.active_program = True
-                stack[-1].extend([x,[]])
-                stack.append(stack[-1][-1])
-            elif x == '>' or x == ']':
-                if self.imp_arrow and x == '>':
-                    stack[-1].append('->')
-                    self.imp_arrow = False
-                else:
-                    stack.pop()
-                    if len(stack[-1][-1]) > 3:
-                        return 'error: to many arguments between parentheses'
-                    if not self.open_brac[self.close_brac[x]]:
-                        return 'error: misaligned program brackets'
-                    self.open_brac[self.close_brac[x]] = False
-                    self.active_program = False
-            elif x == '+' or x == '*' or x == '?' or x == "'":
-                if not self.active_program:
-                    return 'error: program operator used outside program'
-                else:
-                    stack[-1][-1] = [x, stack[-1][-1]]
-            elif x == '(':
-                if self.negation:
-                    self.negation = False
-                    stack[-1].append(['!',[]])
-                    stack.append(stack[-1][-1][-1])
-                else:
-                    stack[-1].append([])
-                    stack.append(stack[-1][-1])
-            elif x == ')':
-                stack.pop()
-                if not stack:
-                    return 'error: opening bracket is missing'
-                if len(stack[-1][-1]) > 3:
-                    return 'error: to many arguments between parentheses'
-            else:
-                # if self.active_program and x not in self.valid_prog_op and x not in self.programs and x not in self.atoms:
-                #     return 'error: invalid character in program'
-                # elif not self.active_program and x not in self.valid_prop_op and x not in self.atoms:
-                #     return 'error: invalid character in formula'
-                # if self.imp_arrow:
-                #     return 'error: invalid formula, expected implication arrow due to -'
-                # else:
-                if self.negation:
-                    stack[-1].append(['!', x])
-                    self.negation = False
-                else:
-                    stack[-1].append(x)
-        if len(stack) > 1:
-            return 'error: closing bracket is missing'
-        if len(stack[0]) > 3:
-            return 'error: invalid formula, press \'h\' for help'
-        return stack.pop()
 
 """
 while True:
@@ -179,3 +114,7 @@ while True:
     ret = c.parse(s)
     print(ret)
 """
+# while True:
+#     s = input("Enter a logical formula: ")
+#     print(FormulaParser(s, ['p','q','r','s'],['a','b','c','d']))
+    # FormulaParser(i_s, ['p','q','r','s'],['a','b','c','d'])
